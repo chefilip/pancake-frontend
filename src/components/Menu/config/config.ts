2429c3ad3f5d8ddcee07ac9086835bf895e8340b
Chef Cheems <chefcheems@protonmail.com>
2022-02-07 22:44:52 +0200

    feat: Add menu links

--- src/components/Menu/config/config.ts
+++ src/components/Menu/config/config.ts
@@ -32,6 +32,10 @@ const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
         label: t('Liquidity'),
         href: '/liquidity',
       },
+      {
+        label: t('Limit Orders'),
+        href: '/limit-orders',
+      },
     ],
   },
   {
